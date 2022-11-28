050bd9997a9439153342abf7ab8476dd643c16a0
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-11-28 10:38:10 +0100

    fix index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+var corsOptions = {
+    origin: "http://localhost:4200"
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const db = require("./utilities/db.js");
+const db = require("./models/db.js");
 db.sequelize.sync();
 require("./routes/customers.routes.js")(app);
 PORT = process.env.PORT || 8080;
